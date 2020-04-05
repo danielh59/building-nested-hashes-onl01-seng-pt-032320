ec5660b2a10c3501a5809c198139cca1e8aab0b1
Daniel Hoyte <daniel.hoyte59@gmail.com>
2020-04-05 21:10:21 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -4,14 +4,14 @@ def second_challenge
       :patriach => {},
       :matriach => {},
       :hero => {}, 
-      :hero_friends => [],
+      :hero_friends => []
       
       }, 
     :capulet => {
      :patriarch => {},
       :matriarch => {},
       :heroine => {},
-      :heroine_friends = [],
+      :heroine_friends = []
       }
     
   }
